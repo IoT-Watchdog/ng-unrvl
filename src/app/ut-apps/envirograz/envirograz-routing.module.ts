7ab8bf7c99fb805f32515542bab75f899e1f8d9a
Michael Maier <michael.maier@unraveltec.com>
2020-05-13 09:16:30 +0200

    rm unneeded sensor apps

--- src/app/ut-apps/envirograz/envirograz-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { EnvirograzComponent } from './envirograz.component';const routes: Routes = [ { path: '', component: EnvirograzComponent }];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class EnvirograzRoutingModule { }
