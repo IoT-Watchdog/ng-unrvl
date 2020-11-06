6edc15a1ebd157c8b1feb11535f7aaa174ff4d35
Michael Maier <michael.maier@unraveltec.com>
2020-11-06 08:07:12 +0100

    rm unneeded tiles from dashboard

--- src/app/ut-dashboard/ut-dashboard.module.ts
+++ src/app/ut-dashboard/ut-dashboard.module.ts
@@ -4,23 +4,11 @@ import { CommonModule } from '@angular/common';
 import { UtDashboardRoutingModule } from './ut-dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UtDashboardTileComponent } from './ut-dashboard-tile/ut-dashboard-tile.component';
-import { BrightnessModule } from '../ut-apps/tiles/brightness/brightness.module';
-import { TileCo2Module } from '../ut-apps/tiles/tile-co2/tile-co2.module';
-import { TileRhModule } from '../ut-apps/tiles/tile-rh/tile-rh.module';
-import { TilePmModule } from '../ut-apps/tiles/tile-pm/tile-pm.module';
-import { TileLoadModule } from '../ut-apps/tiles/tile-load/tile-load.module';
-import { TileUvModule } from '../ut-apps/tiles/tile-uv/tile-uv.module';
 
 @NgModule({
   imports: [
     CommonModule,
     UtDashboardRoutingModule,
-    BrightnessModule,
-    TileCo2Module,
-    TileRhModule,
-    TilePmModule,
-    TileLoadModule,
-    TileUvModule,
   ],
   declarations: [DashboardComponent, UtDashboardTileComponent],
 })
